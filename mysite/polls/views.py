047f50e50908a4756d481074087d194a788c82f3
Praveen <praveen@testpress.in>
2017-10-09 19:58:00 +0530

    Django Tutorial:
    -Tutorial 4
    -Tutorial 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,7 +40,7 @@ from django.views import generic
 #	return render(request, 'polls/results.html',{'question': question})
 #
 
-
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -48,12 +48,19 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+	
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
